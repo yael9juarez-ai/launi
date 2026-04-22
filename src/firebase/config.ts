3c16f4e57c2dcbac25292aa8fc3d2eaa0ec5421a
Yael Acosta <yael.9juarez@gmail.com>
2026-04-22 15:57:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5360630258-42a33",
+  "appId": "1:654890718584:web:73be6cd457ecc2134aa48d",
+  "apiKey": "AIzaSyDVpM9Y6uPTOusDRj5uvOHHWxVy1sdN_mE",
+  "authDomain": "studio-5360630258-42a33.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "654890718584"
+};
